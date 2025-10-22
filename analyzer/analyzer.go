b7a7ad0055a4e6fc1463479e21665aaef754742d
ahan-adelaide <a1965990@adelaide.edu.au>
2025-10-22 20:26:11 +1030

    Add checking for vulnerable functions

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -5,6 +5,7 @@ package analyzer
 import (
 	"flag"
 	"fmt"
+	"go/ast"
 	"slices"
 	"strconv"
 
@@ -18,7 +19,6 @@ var ecImportPaths = []string{
 	"crypto/ecdsa",
 	"crypto/ed25519",
 	"crypto/elliptic",
-	"crypto/tls",
 }
 
 // Imports that are quantum-vulnerable because they
@@ -26,7 +26,29 @@ var ecImportPaths = []string{
 var ifImportPaths = []string{
 	"crypto/rsa",
 	"crypto/dsa",
-	"crypto/tls",
+}
+
+// Imports that are quantum-vulnerable because they
+// implement a quantum-vulnerable key exchange algorithm
+// that can be replaced by "crypto/mlkem"
+var keyExchangePaths = []string{
+	"crypto/ecdh",
+}
+
+// Identifiers of functions that implement quantum-vulnerable algorithms.
+var fnIdentifiers = []string{
+	"DecryptOAEP",
+	"DecryptPKCS1v15",
+	"DecryptPKCS1v15SessionKey",
+	"EncryptOAEP",
+	"EncryptPKCS1v15",
+	"SignPKCS1v15",
+	"SignPSS",
+	"VerifyPKCS1v15",
+	"VerifyPSS",
+	"SignASN1",
+	"VerifyASN1",
+	"NewTripleDESCipher",
 }
 
 func pqcAnalyze(pass *analysis.Pass) (any, error) {
@@ -40,7 +62,30 @@ func pqcAnalyze(pass *analysis.Pass) (any, error) {
 				pass.Reportf(currImport.Pos(), "%s uses quantum-vulnerable elliptic curve cryptography", currImport.Path.Value)
 			}
 			if slices.Contains(ifImportPaths, importPath) {
-				pass.Reportf(currImport.Pos(), "%s uses quantum-vulnerable integer factorization cryptography", currImport.Path.Value)
+				pass.Reportf(currImport.Pos(), "%s uses quantum-vulnerable integer factorization cryptography. use \"crypto/mlkem\" instead.", currImport.Path.Value)
+			}
+		}
+		
+		for _, decl := range file.Decls {
+			funcDecl, ok := decl.(*ast.FuncDecl)
+			if !ok {
+				continue
+			}
+			
+			if funcDecl.Body != nil {
+				continue
+			}
+			
+			for _, token := range funcDecl.Body.List {
+				if assignment, ok := token.(*ast.AssignStmt); ok {
+					for _, expr := range assignment.Rhs {
+						if callExpr, ok := expr.(*ast.CallExpr); ok {
+							if slices.Contains(fnIdentifiers, callExpr.Fun.(*ast.Ident).Name) {
+								pass.Reportf(callExpr.Fun.Pos(), "function %s is quantum-vulnerable", callExpr.Fun.(*ast.Ident).Name)
+							}
+						}
+					}
+				}
 			}
 		}
 	}
